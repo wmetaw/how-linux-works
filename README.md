31e19bcf6bfc7f1700087da8bc71fbe89303b5dd
Ryo Takashima <r.takashima@miniascape.co.jp>
2018-04-08 13:18:01 +0900

    hello

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# USAGE
+# Usage
 
 ```
 $ cd $HOME/work
 $ git clone git@github.com:wmetaw/how-linux-works.git
+$ cd how-linux-works
 $ mkdir work
-$ docker image pull wmetaw/normallinux 
-$ docker run -v $HOME/work/how-linux-works/work:/usr/local/src -d wmetaw/normallinux
+$ docker image pull wmetaw/normallinux
+
+# work以下をdockerにマウント
+$ docker run -v $HOME/work/how-linux-works/work:/usr/local/src -itd wmetaw/normallinux /bin/bash
+=> <CONTAINER ID>
+
 $ docker attach <CONTAINER ID>
 ```
---
+++ work/hello.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+  puts("hello world");
+  return 0;
+}
+
+/**
+$ cat hello.log
+
+brk( NULL) = 0 x 917000
+access("/ etc/ ld. so. nohwcap", F_ OK) = -1
+ENOENT (No such file or directory)
+mmap( NULL, 8192, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0) = 0x7f3ff46c2000
+access("/ etc/ ld. so. preload", R_OK) = -1
+ENOENT (No such file or directory)
+...
+brk(NULL) = 0x917000
+brk(0x938000) = 0x938000
+write(1, "hello world\n", 12) = 12 ←①
+exit_group( 0) = ?
++++ exited with 0 +++
+
+**/
